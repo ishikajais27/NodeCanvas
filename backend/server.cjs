16f53d43a4e083ad24a702cd9a3a11852e97a56e
ishikajais27 <ishikajais09876@gmail.com>
2025-06-19 23:43:22 +0530

    Deploy backend part on render

--- backend/server.cjs
+++ backend/server.cjs
@@ -1,27 +1,10 @@
-// const express = require('express')
-// const cors = require('cors')
-// const apiRouter = require('./routes/api.cjs')
-
-// const app = express()
-// const PORT = 5000
-
-// app.use(cors())
-// app.use(express.json())
-
-// // API routes
-// app.use('/api', apiRouter)
-
-// // Start server
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`)
-// })
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const apiRouter = require('./routes/api.cjs')
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
