896b10a78b66f1b87915163db9eaefda1b6ce158
ishikajais27 <ishikajais09876@gmail.com>
2025-06-19 22:03:06 +0530

    fix bugs

--- backend/server.cjs
+++ backend/server.cjs
@@ -21,7 +21,7 @@ const path = require('path')
 const apiRouter = require('./routes/api.cjs')
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = 5000
 
 app.use(cors())
 app.use(express.json())
